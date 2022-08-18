7d2a84b926e51cb9aa2ea47c79150b184970c983
Dalton Gutierrez <daltonguti@gmail.com>
2022-08-17 21:29:55 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+		<link rel="stylesheet/less" type="text/css" href="assets/custom.less" />
+	</head>
+	<body>
+	<div class="title">	
+		<div class="from-right">JULIETA,</div>
+		<div class="from-bottom">Te <span class="love">amo</span> mucho</div>
+	</div>
+	<!-- Button trigger modal -->
+	<button id="btnCustom" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+	  CLICK PARA VER ALGO HERMOSO
+	</button>
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+		  <div class="modal-body">
+			<img class="imagen" src="images/picture.jpg" />
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		  </div>
+		</div>
+	  </div>
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/less" ></script>
+	<script type="text/javascript" src="assets/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="assets/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+	</body>
+</html>
